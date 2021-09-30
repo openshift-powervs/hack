06a491ac977a29b5d66d797e109f83a8fd936710
Manjunath Kumatagi <mkumatag@in.ibm.com>
2021-09-30 14:25:17 -0400

    latest images
    the latest image overrides, as well as a slight restructure to make
    moving things around a little easier.
    
    and i dump the image name for later use
    
    Signed-off-by: Manjunath Kumatagi <mkumatag@in.ibm.com>

--- scripts/ocp-release.sh
+++ scripts/ocp-release.sh
@@ -14,18 +14,24 @@ TO_IMAGE_REGISTRY=${TO_IMAGE_REGISTRY:-quay.io/powercloud}
 TO_IMAGE=${TO_IMAGE:-openshift-release:4.9-powervs}-${TIMESTAMP}
 MAX_PER_REGISTRY=${MAX_PER_REGISTRY:-8}
 
-oc adm release new \
+MCO_IMAGE=${MCO_IMAGE:-quay.io/powercloud/ose-machine-config-operator@sha256:685aa4e4cd77ba325cc361f868b804f4fbf561b5b4385cbb0ee7dd01d2cc1756}
+
+oc adm release new --max-per-registry=${MAX_PER_REGISTRY} --allow-missing-images --insecure -a ~/pull_secret.json \
 --from-release=${FROM_RELEASE} \
 --to-image=localhost:5000/${TO_IMAGE} \
-machine-config-operator=quay.io/powercloud/ose-machine-config-operator@sha256:685aa4e4cd77ba325cc361f868b804f4fbf561b5b4385cbb0ee7dd01d2cc1756 \
-cluster-kube-apiserver-operator=quay.io/powercloud/openshift-cluster-kube-apiserver-operator@sha256:cab8b9d07db9ffe91a0c50e847f3544b9f3bb937f513293776bcd5d93f72205e \
-cluster-etcd-operator=quay.io/powercloud/openshift-etcd-operator@sha256:0a73fc0ad29be7fe9c7a1ba8a9308fd5cdb5b7c315dabc013937767e347ac41e \
-cluster-config-operator=quay.io/powercloud/openshift-cluster-config-operator@sha256:4d98cd7416a4e39293f87073560d23e844acdc205d416b739ff72a42d2112b11 \
-hyperkube=quay.io/powercloud/hyperkube@sha256:4978196798db5a3645b1982478f0af03e9b6ca4d2b97da26ee5579ec8d89d779 \
-machine-os-content=quay.io/powercloud/machine-os-content@sha256:cbed704ff0c60955c5faa0ecb7bc40c77f4398355a1740ff28112a9956c1f229 \
-machine-api-operator=quay.io/powercloud/machine-api-operator@sha256:df5e9f413918e6a64a1d59a510ef0e9ce90c190155e18a7fdd2ccf117fcf0602 \
---max-per-registry=${MAX_PER_REGISTRY} --allow-missing-images --insecure -a ~/pull_secret.json
+machine-config-operator=${MCO_IMAGE} \
+machine-api-operator=quay.io/psundara/ose-machine-api-operator@sha256:ab24a19ff29f72364041d40af75afc3f010cb2402e0b87949884b9804fab1bf3
+
+#machine-api-operator=quay.io/powercloud/machine-api-operator@sha256:6015d050ad6b5be4f4ab66833ee59d40fb17b9ac7033551c72a93d6d7d2991f0
+#cluster-kube-apiserver-operator=quay.io/powercloud/openshift-cluster-kube-apiserver-operator@sha256:cab8b9d07db9ffe91a0c50e847f3544b9f3bb937f513293776bcd5d93f72205e \
+#cluster-etcd-operator=quay.io/powercloud/openshift-etcd-operator@sha256:0a73fc0ad29be7fe9c7a1ba8a9308fd5cdb5b7c315dabc013937767e347ac41e \
+#cluster-config-operator=quay.io/powercloud/openshift-cluster-config-operator@sha256:4d98cd7416a4e39293f87073560d23e844acdc205d416b739ff72a42d2112b11 \
+#hyperkube=quay.io/powercloud/hyperkube@sha256:4978196798db5a3645b1982478f0af03e9b6ca4d2b97da26ee5579ec8d89d779 \
+#machine-os-content=quay.io/powercloud/machine-os-content@sha256:cbed704ff0c60955c5faa0ecb7bc40c77f4398355a1740ff28112a9956c1f229
 
 podman pull localhost:5000/${TO_IMAGE}
 podman tag localhost:5000/${TO_IMAGE} ${TO_IMAGE_REGISTRY}/${TO_IMAGE} 
 podman push --authfile ~/pcloud_bot_key.json ${TO_IMAGE_REGISTRY}/${TO_IMAGE}
+
+# or look for the newest img at https://quay.io/repository/powercloud/openshift-release?tab=tags
+echo "${TO_IMAGE_REGISTRY}/${TO_IMAGE}" > latest_release_img
